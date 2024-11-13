63befb344282dc67ae2c43e0521b7ecdca39e9d6
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-11-13 11:35:41 +0530

    demo_pro

--- demo_pro/demo_app/views.py
+++ demo_pro/demo_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 # Create your views here.
+
+
+def demo_login(req):
+    if req.method=='POST':
+        uname=req.POST['uname']
+        password=req.POST['password']
+        data=authenticate(username=uname,password=password)
+        if data:
+            login(req,data)
+            return redirect(shop_home)
+        else:
+            messages.warning(req,'invalid username and password')
+            return redirect(demo_login)
+    else:
+        return render(req,'login.html')
+       
+def shop_home(req):
+    return render(req,'shop/home.html')
