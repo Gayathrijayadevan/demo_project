81a6eb7ccd6a14822c4e3ffdc23f0d7c346aad2d
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-11-14 11:39:38 +0530

    demo

--- demo_pro/demo_app/urls.py
+++ demo_pro/demo_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.demo_login),
-    path('shop_home',views.shop_home)
+    path('shop_home',views.shop_home),
+    path('logout',views.demo_login),
 ]
--- demo_pro/demo_app/views.py
+++ demo_pro/demo_app/views.py
@@ -5,18 +5,29 @@ from django.contrib import messages
 
 
 def demo_login(req):
+    if 'shop' in req.session:
+        return redirect(shop_home)
     if req.method=='POST':
         uname=req.POST['uname']
         password=req.POST['password']
         data=authenticate(username=uname,password=password)
         if data:
             login(req,data)
+            req.session['shop']=uname #creating a session
             return redirect(shop_home)
         else:
-            messages.warning(req,'invalid username and password')
-            return redirect(demo_login)
+            messages.warning(req,'invalid username or password') #to display error message
+            return redirect(demo_login)  
     else:
         return render(req,'login.html')
+    
+def demo_shop_logout(req):
+    logout(req)
+    req.session.flush() #for deleting the session
+    return redirect(demo_login)
        
 def shop_home(req):
-    return render(req,'shop/home.html')
+    if 'shop' in req.session:
+        return render(req,'shop/home.html')
+    else:
+        return redirect(demo_login)
