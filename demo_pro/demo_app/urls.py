8bad8715c53e237cebb3376db1b35a0d44d680a5
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-11-15 11:27:37 +0530

    demo_pro

--- demo_pro/demo_app/urls.py
+++ demo_pro/demo_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     path('',views.demo_login),
     path('shop_home',views.shop_home),
-    path('logout',views.demo_login),
+    path('logout',views.demo_shop_logout),
+    path('add',views.add_products)
 ]
--- demo_pro/demo_app/views.py
+++ demo_pro/demo_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .models import *
 # Create your views here.
 
 
@@ -28,6 +29,26 @@ def demo_shop_logout(req):
        
 def shop_home(req):
     if 'shop' in req.session:
-        return render(req,'shop/home.html')
+        data=Product.objects.all()
+        return render(req,'shop/home.html',{'Products':data})
     else:
         return redirect(demo_login)
+
+def add_products(req) :
+    if 'shop' in req.session:
+        if req.method=='POST':
+            pid=req.POST['pid']
+            pname=req.POST['name']
+            des=req.POST['descrip']
+            pprice=req.POST['price']
+            oprice=req.POST['off_price']
+            pstock=req.POST['stock']
+            file=req.FILES['img'] #to get the img from html page we  are using file method instead of post and adding enctype in html page
+
+            data=Product.objects.create(pid=pid,name=pname,des=des,price=pprice,offer_price=oprice,stock=pstock,img=file)
+            data.save()
+            return redirect(shop_home)
+        else:
+            return render(req,'shop/add_products.html')
+    else:
+        return redirect(demo_login)    
