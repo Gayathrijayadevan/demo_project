b4cd66a74b81b132eb3fb2159a7bcd0a243f2aaf
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-12-03 13:23:29 +0530

    smtp email

--- demo_pro/demo_pro/settings.py
+++ demo_pro/demo_pro/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gayathrijayadevan11@gmail.com'#sender's email-id
+EMAIL_HOST_USER = ''#sender's email-id
 EMAIL_HOST_PASSWORD = 'vxow8korh2prxu1hydm'#password associated with above email-id (not the regular password)
